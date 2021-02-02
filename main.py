29198a2b64f0efed34e8df14e5fa0f327935d7a8
scrubjay55 <karayay7@gmail.com>
2021-02-02 23:20:01 +0300

    update

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 from dhooks import Webhook
-from Reddit_ChatBot_Python.ChatBot import ChatBot
+from Reddit_ChatBot_Python import ChatBot, RedditAuthentication
 
 
+dc_webhook_url = ""
+
+reddit_bot_authentication = RedditAuthentication.PasswordAuth(reddit_username="", reddit_password="", twofa="")
+chatbot = ChatBot(print_chat=True, store_session=True, authentication=reddit_bot_authentication)
+
+hook = Webhook(dc_webhook_url)
+
+
+@chatbot.after_message_hook(frame_type='MESG')
 def send2discord(resp):
-    if resp.type_f == "MESG":
-        to_send = f"{resp.user.name}: {resp.message}"
-        hook.send(to_send)
-
-
-if __name__ == "__main__":
-    sub_channels = ["Turkey"]
-    dc_webhook_url = ""
-    reddit_api_token = ""
-
-    hook = Webhook(dc_webhook_url)
-    chatbot = ChatBot(reddit_api_token=reddit_api_token, sub_channels=sub_channels, print_chat=True, store_session=True)
-    websock = chatbot.WebSocketClient
-
-    websock.add_after_message_hook(send2discord)
-
-    while True:
-        try:
-            websock.run_4ever()
-        except:
-            continue
+    hook.send(f"{resp.user.name}@{chatbot.get_chatroom_name_id_pairs().get(resp.channel_url)}: {resp.message}")
